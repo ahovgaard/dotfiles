2277f3f30a85aba6235a565292b71bb842522c30
Anders Kiel Hovgaard <akhovg@gmail.com>
2016-07-21 19:05:15 +0900

    added eye candy scrren lock script

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+tmpbg='/tmp/screen.png'
 
-i3lock -c 000000
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+#[[ -f $1 ]] && convert "$tmpbg" $1 -gravity center -composite -matte "$tmpbg"
+i3lock -i "$tmpbg"
+rm "$tmpbg"
