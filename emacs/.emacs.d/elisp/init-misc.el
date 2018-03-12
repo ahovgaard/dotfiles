80562596a653ad51614f4fa1d6e774748184b335
Anders Kiel Hovgaard <akhovg@gmail.com>
2018-03-12 22:37:26 +0100

    Useful little "free feature" from Endless Parentheses.

--- emacs/.emacs.d/elisp/init-misc.el
+++ emacs/.emacs.d/elisp/init-misc.el
@@ -22,4 +22,19 @@
     (delete-file (buffer-file-name))
     (kill-this-buffer)))
 
+;; Fill and unfill paragraphs with a single key
+;; (from http://endlessparentheses.com)
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
+
 (provide 'init-misc)
