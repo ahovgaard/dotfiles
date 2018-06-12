e99c25e3ca5582c64844d9d3485fddc861369d2d
Anders Kiel Hovgaard <akhovg@gmail.com>
2018-06-12 11:43:07 +0200

    Config for pdf-tools and proof general.

---
+++ emacs/.emacs.d/elisp/init-pdf.el
@@ -0,0 +1,6 @@
+(use-package pdf-tools
+  :ensure t
+  :config
+  (pdf-tools-install))
+
+(provide 'init-pdf)
---
+++ emacs/.emacs.d/elisp/init-pg.el
@@ -0,0 +1,4 @@
+;; Open .v files with Proof General's Coq mode.
+(load "~/.emacs.d/elisp/PG/generic/proof-site")
+
+(provide 'init-pg)
