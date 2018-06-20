0fcd80f279d43041530433731128e74c5e1966ff
Anders Kiel Hovgaard <akhovg@gmail.com>
2018-06-20 17:06:34 +0200

    A few changes to emacs LaTeX config.

--- emacs/.emacs.d/elisp/init-tex.el
+++ emacs/.emacs.d/elisp/init-tex.el
@@ -2,6 +2,7 @@
   :ensure auctex
   :config
   (setq font-latex-fontify-script nil)
+  (setq TeX-save-query nil)
 
   (add-hook 'LaTeX-mode-hook
             (lambda () (set-fill-column 80))))
@@ -19,5 +20,19 @@
 (setq reftex-plug-into-AUCTeX t)
 
 (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+
+(general-define-key
+ :states '(normal visual insert emacs)
+ :prefix "SPC"
+ :non-normal-prefix "M-SPC"
+
+ "mc"  'TeX-command-master
+ "mr"  '(:ignore t :which-key "RefTeX")
+ "mrc" 'reftex-citation
+ "mrg" 'reftex-grep-document)
+
+;; use zathura to view pdf documents
+(add-to-list 'TeX-view-program-list '("zathura" "zathura %o"))
+(setq TeX-view-program-selection '((output-pdf "zathura")))
 
 (provide 'init-tex)
