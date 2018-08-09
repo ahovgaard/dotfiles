7a4910496ad4e58cbbdcdb00beb9f8447fd92132
Anders Kiel Hovgaard <akhovg@gmail.com>
2018-08-09 22:17:57 +0200

    Add keybinding for alchemist goto definition.

--- emacs/.emacs.d/elisp/init-elixir.el
+++ emacs/.emacs.d/elisp/init-elixir.el
@@ -1,4 +1,10 @@
 (use-package alchemist
   :ensure t)
 
+(general-define-key
+ :states '(normal visual)
+ :keymaps 'alchemist-mode-map
+ "gd" 'alchemist-goto-definition-at-point
+ )
+
 (provide 'init-elixir)
