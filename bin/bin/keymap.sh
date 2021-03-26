1e6a9acb23890ce1b1d19c40c1f9254b196493a0
Anders Kiel Hovgaard <KVQ384@motorolasolutions.com>
2021-03-26 15:20:56 +0100

    Improve keymap script.

--- bin/bin/keymap.sh
+++ bin/bin/keymap.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # Map escape to caps lock and set US international layout.
-setxkbmap -option caps:escape
-setxkbmap -layout us -variant altgr-intl
 
-if [ $(hostname) == 'zdk02-4g43' ]; then
-  setxkbmap -option ctrl:menu_rctrl
-  setxkbmap -option ctrl:swap_rwin_rctl
+if lsusb | grep Ergodox; then
+    echo "Skipping keymap config since Ergodox is connected..."
+else
+    echo "Set key mapping since Ergo not connected..."
+    setxkbmap -option caps:escape
+    setxkbmap -layout us -variant altgr-intl
+
+    if [ "$(hostname)" = 'zdk02-4g43' ]; then
+        setxkbmap -option ctrl:menu_rctrl
+        setxkbmap -option ctrl:swap_rwin_rctl
+    fi
 fi
+
