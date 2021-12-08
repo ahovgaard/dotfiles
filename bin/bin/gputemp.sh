3de673190770b9ed3946783f1d4e47151b11e90f
Anders Kiel Hovgaard <me@ahovgaard.dk>
2021-12-08 21:12:52 +0100

    Fix gputemp script.

--- bin/bin/gputemp.sh
+++ bin/bin/gputemp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if ! gputemp=$(nvidia-smi --format=nounits,csv,noheader --query-gpu=temperature.gpu | xargs echo); then
+if ! gputemp=$(nvidia-smi --format=nounits,csv,noheader --query-gpu=temperature.gpu 2> /dev/null | xargs echo); then
     gputemp=0
 fi
 if [ "$gputemp" -gt 0 ]; then
     echo "$gputemp°C"
 else
-    echo "no nvidia driver installed"
+    echo "OFF"
 fi
